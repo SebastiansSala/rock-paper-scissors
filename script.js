a56b9f043c3d7d6cbb4493c93758d143531e6447
Sebastian Sala <sebastian_orko@hotmail.com>
2023-02-28 13:15:23 -0600

    rock-paper-scissors first script

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 function getComputerChoice(){
-    return ["rock", "paper", "scissors"].random()
+    const arr = ["rock", "paper", "scissors"];
+    return arr[(Math.floor(Math.random() * arr.length))];
 }
 
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toUpperCase();
-    if((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
-        return "You Win!";
-    }else{
-        return "You Lose!";
+    if(playerSelection === "rock"){
+        if(computerSelection === "rock"){
+            return "Its a Tie!"
+        }else if(computerSelection === "paper"){
+            return "You Lose!"
+        }else if(computerSelection === "scissors"){
+            return "You win!"
+        }
+    }
+
+    if(playerSelection === "paper"){
+        if(computerSelection === "paper"){
+            return "Its a Tie!"
+        }else if(computerSelection === "scissors"){
+            return "You Lose!"
+        }else if(computerSelection === "rock"){
+            return "You win!"
+        }
+    }
+
+    if(playerSelection === "scissors"){
+        if(computerSelection === "scissors"){
+            return "Its a Tie!"
+        }else if(computerSelection === "rock"){
+            return "You Lose!"
+        }else if(computerSelection === "paper"){
+            return "You win!"
+        }
     }
 }
 
 function game(){
     for(let i = 0; i < 5; i++){
-        console.log(playRound)
+        return playRound(playerSelection, computerSelection);
     }
 }
-
-
-const playerSelection = "paper";
+const playerSelection = prompt("Please enter your name ");
 const computerSelection = getComputerChoice();
 
+console.log(game());
